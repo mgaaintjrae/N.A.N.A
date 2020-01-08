880b2fca66bec580c6581a5e11d29526f8647623
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-01-08 21:58:24 +0100

    first commit

---
+++ pages/Commons/menu.php
@@ -0,0 +1,58 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark perso_size20">
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle perso_colorRoseMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    L'asso
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item perso_colorRoseMenu" href="#">Action</a>
+                    <a class="dropdown-item perso_colorRoseMenu" href="#">Another action</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle perso_colorOrangeMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Pensionnaires
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item perso_colorOrangeMenu" href="#">Action</a>
+                    <a class="dropdown-item perso_colorOrangeMenu" href="#">Another action</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle perso_colorVertMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Actus
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item perso_colorVertMenu" href="#">Action</a>
+                    <a class="dropdown-item perso_colorVertMenu" href="#">Another action</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle perso_colorRougeMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Conseils
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item perso_colorRougeMenu" href="#">Action</a>
+                    <a class="dropdown-item perso_colorRougeMenu" href="#">Another action</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle perso_colorBleurCielMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Contacts
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item perso_colorBleurCielMenu" href="#">Action</a>
+                    <a class="dropdown-item perso_colorBleurCielMenu" href="#">Another action</a>
+                </div>
+            </li>
+
+        </ul>
+
+    </div>
+</nav>
---
+++ pages/Global/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Fredoka+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Copse&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container p-0 mt-2 rounded perso_shadow">
+        <header class="bg-dark text-white rounded-top perso_policeTitre perso_shadow">
+            <div class="row align-items-center m-0">
+                <div class="col-2 p-2 text-center">
+                    <a href="index.php">
+                        <img src="../../sources/images/Autres/logoNANA2.jpg" class="rounded-circle img-fluid perso_logoSize" alt="logo du site">
+                    </a>
+                </div>
+                <div class="col-8 m-0 p-0">
+                    <?php include("../Commons/menu.php")?>
+                </div>
+                <div class="col-2 text-right pt-1 pr-4 d-none d-lg-block">
+                    N.A.N.A <br /> Clermont (09)
+                </div>
+            </div>
+        </header>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="../../bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
