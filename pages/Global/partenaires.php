c74a76d4bfb77443114a6de31c954d9b0a264ac6
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-01-09 14:28:21 +0100

    page partenaires

--- pages/Global/partenaires.php
+++ pages/Global/partenaires.php
@@ -1,3 +1,39 @@
-<?php include("../Commons/header.php")?>
-Page des partenaires de l'association
-<?php include("../Commons/footer.php")?>
+<?php include("../Commons/header.php") ?>
+<h2 class="text-center mt-3 perso_colorRoseMenu perso_policeTitre perso_textShadow">Les partenaires</h2>
+
+<div class="row no-gutters">
+    <div class="card col-auto mx-auto mt-3" style="width: 18rem;">
+        <img src="../../sources/images/Autres/updp-logo.png" class="card-img-top p-1" alt="UPDP">
+        <div class="card-body text-center">
+            <h5 class="card-title perso_colorRoseMenu perso_policeTitre perso_textShadow">UPDP 09</h5>
+            <p class="card-text">Contacter un éducateur canin, contactez UPDP 09</p>
+            <a href="http://www.updp-09.fr" target="_blank" class="btn btn-primary">Visiter le site de l'éducatrice</a>
+        </div>
+    </div>
+    <div class="card col-auto mx-auto mt-3" style="width: 18rem;">
+        <img src="../../sources/images/Autres/updp-logo.png" class="card-img-top p-1" alt="UPDP">
+        <div class="card-body text-center">
+            <h5 class="card-title perso_colorRoseMenu perso_policeTitre perso_textShadow">UPDP 09</h5>
+            <p class="card-text">Contacter un éducateur canin, contactez UPDP 09</p>
+            <a href="http://www.updp-09.fr" target="_blank" class="btn btn-primary">Visiter le site de l'éducatrice</a>
+        </div>
+    </div>
+    <div class="card col-auto mx-auto mt-3" style="width: 18rem;">
+        <img src="../../sources/images/Autres/updp-logo.png" class="card-img-top p-1" alt="UPDP">
+        <div class="card-body text-center">
+            <h5 class="card-title perso_colorRoseMenu perso_policeTitre perso_textShadow">UPDP 09</h5>
+            <p class="card-text">Contacter un éducateur canin, contactez UPDP 09</p>
+            <a href="http://www.updp-09.fr" target="_blank" class="btn btn-primary">Visiter le site de l'éducatrice</a>
+        </div>
+    </div>
+    <div class="card col-auto mx-auto mt-3" style="width: 18rem;">
+        <img src="../../sources/images/Autres/updp-logo.png" class="card-img-top p-1" alt="UPDP">
+        <div class="card-body text-center">
+            <h5 class="card-title perso_colorRoseMenu perso_policeTitre perso_textShadow">UPDP 09</h5>
+            <p class="card-text">Contacter un éducateur canin, contactez UPDP 09</p>
+            <a href="http://www.updp-09.fr" target="_blank" class="btn btn-primary">Visiter le site de l'éducatrice</a>
+        </div>
+    </div>
+</div>
+
+<?php include("../Commons/footer.php") ?>
